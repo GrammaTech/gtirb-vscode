88826477f3a3ae50c309a20ebde543b972aac295
Timothy Neale <tneale@grammatech.com>
2021-10-20 16:55:44 -0400

    Include LSP server

---
+++ server/__main__.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3 
+# -*- coding: utf-8 -*- 
+
+import os
+import json
+import logging
+import argparse
+import gtirb
+import re
+from collections import defaultdict
+from typing import List, Text, Tuple
+from pygls.server import LanguageServer
+from pygls.protocol import LanguageServerProtocol
+from pygls.lsp.methods import (
+    TEXT_DOCUMENT_DID_OPEN,
+    TEXT_DOCUMENT_DID_CLOSE,
+    TEXT_DOCUMENT_DID_SAVE,
+    TEXT_DOCUMENT_DID_CHANGE,
+    REFERENCES
+)
+from pygls.lsp.types import (
+    DidOpenTextDocumentParams,
+    DidSaveTextDocumentParams,
+    DidCloseTextDocumentParams,
+    DidChangeTextDocumentParams,
+    ReferenceParams,
+)
+
+DEFAULT_PORT = 3036
+DEFAULT_TCP_FLAG = True
+DEFAULT_STDIO_FLAG = False
+
+current_documents = {} # not sure defaultdict would support factory of TextDocument?
+current_indexes = {} # not sure defaultdict would support factory of TextDocument?
+
+class NonTerminatingLanguageServerProtocol(LanguageServerProtocol):
+    """
+    language server protocol implementation which ignores
+    exit/shutdown/connection_lost messages from the client
+    """
+
+    def bf_exit(self, *args):
+        pass
+
+    def bf_shutdown(self, *args):
+        pass
+
+    def connection_lost(self, *args):
+        pass
+
+class Index:
+
+    def __init__ (self, gtirb, asm, xref, defs):
+        self.gtirb = gtirb
+        self.asm = asm
+        self.xref = xref
+        self.defs = defs
+
+    def dump_to_file (self, json_filename):
+        x = {
+            "gtirb": self.gtirb,
+            "asm": self.asm,
+            "xref": self.xref,
+            "defs": self.defs
+        }
+        try:
+            with open(json_filename, "w") as out:
+                json.dump(x, out, indent=4)
+        except Exception as inst:
+            print(inst)
+            print("Unable to write cross-reference file %s." % json_filename)
+            quit()
+        print("\nWrote cross reference file: %s.\n" % json_filename)
+
+#
+# chars to strip out so as to leave a line consisting of actual tokens
+delims = ['+', '-', '[', ']', ':', '{', '}', '*', ',']
+def replace_delims(line):
+    for ch in delims:
+        line = line.replace(ch, ' ')
+    return line
+
+def do_indexing(text_document):
+    path = text_document.uri.split('//')
+    lines = text_document.text.splitlines()
+
+    if len(path) > 1 and path[0] == 'file:':
+        asmfile = path[1]
+        gtirbfile = os.path.splitext(asmfile)[0];
+        jsonfile = asmfile+'.json'
+    else:
+        print(f"error in text document path: {text_document.uri}")
+        return
+
+    #
+    # Get list of symbols from GTIRB file
+    try:
+        ir = gtirb.IR.load_protobuf(gtirbfile)
+    except Exception as inst:
+        print(inst)
+        print("Unable to load gtirb file %s." % gtirbfile)
+        return
+
+    modules = ir.modules
+    module = next(iter(modules))
+    symbols = module.symbols
+    symlist = []
+    for symbol in symbols:
+        symlist.append(symbol.name)
+
+    # if this is a definition, a symbol followed by a colon is the whole line
+    def_search = re.compile('^.*:$')
+
+    #
+    # Process the assembly code line by line
+    defs = {}
+    refs = defaultdict(list)
+    for i, line in enumerate(lines):
+        if def_search.match(line) != None:
+            defined_symbol = line[:-1]
+            print("looking for %s..." % defined_symbol)
+            if defined_symbol in symlist:
+                defs[defined_symbol] = i
+                print("found def of %s at %d" % (defined_symbol, i))
+    
+        # parse the tokens to see if any are a symbol
+        for word in replace_delims(line).split():
+            if word in symlist:
+                #print("found reference to %s at line %d" % (word, i))
+                refs[word].append(i)
+
+    index = Index(gtirbfile, asmfile, refs, defs)
+    index.dump_to_file(jsonfile) 
+
+    #
+    # Add to current indexes
+    current_indexes[text_document.uri] = index
+        
+
+def main():
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--tcp",
+        action="store_true",
+        default=DEFAULT_TCP_FLAG,
+        help="Run server in TCP mode.",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=DEFAULT_PORT,
+        help="Port to listen for requests on.",
+    )
+    parser.add_argument(
+        "--stdio",
+        action="store_true",
+        default=DEFAULT_STDIO_FLAG,
+        help="Run server in STDIO mode.",
+    )
+
+    args = parser.parse_args()
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+
+    if args.stdio:
+        server = LanguageServer()
+    else:
+        server = LanguageServer(protocol_cls=NonTerminatingLanguageServerProtocol)
+
+    @server.feature(TEXT_DOCUMENT_DID_CHANGE)
+    def did_change(ls, params: DidChangeTextDocumentParams):
+        """Text document did change notification."""
+        print(f"Text Document Did Change notification, uri: {params.text_document.uri}")
+        # extraneous # return None
+
+
+    @server.feature(TEXT_DOCUMENT_DID_OPEN)
+    async def did_open(ls, params: DidOpenTextDocumentParams):
+        """Text document did open notification."""
+        print(f"Text Document Did Open notification, uri: {params.text_document.uri}")
+        #print(params)
+        #ext = os.path.splitext(params.text_document.uri)[1];
+        splitpath = os.path.splitext(params.text_document.uri)
+        ext = splitpath[1]
+        print(f"scheme: {splitpath[0]}")
+        print(f"ext: {ext}")
+        print(splitpath)
+
+        # This is where to check the extension
+        if ext == '.gtx86' or ext == '.gtx64' or ext == '.gtmips' or ext == '.gtarm':
+            current_documents[params.text_document.uri] = params.text_document
+            print('Added to document list')
+            do_indexing(params.text_document)
+            print('finished indexing')
+
+
+    @server.feature(TEXT_DOCUMENT_DID_CLOSE)
+    def did_close(ls, params: DidCloseTextDocumentParams):
+        """Text document did close notification."""
+        print(f"Text Document Did Close notification, uri: {params.text_document.uri}")
+        if params.text_document.uri in current_documents:
+            #del current_documents[params.text_document.uri]
+            ls.show_message("document is in current list, you should have removed it just now?")
+
+
+    # returns Optional[List[Location]]
+    @server.feature(REFERENCES)
+    async def get_references(ls, params: ReferenceParams):
+        """Text document references request."""
+        print(f"References request received uri: {params.text_document.uri}")
+        print(f"position (line,char): {params.position.line},{params.position.character}")
+        if params.text_document.uri in current_documents:
+             text_document = current_documents[params.text_document.uri]
+             text  = text_document.text
+             lines = text.splitlines()
+             line = lines[params.position.line]
+             print(f"seeing this line there: {line}")
+        print(params)
+        return None
+
+    # Spawn the server.
+    if args.stdio:
+        server.start_io()
+    else:
+        server.start_tcp("0.0.0.0", args.port)
+
+
+if __name__ == "__main__":
+    main()
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,95 @@
+//"use strict";
+
+import * as net from "net";
+import * as path from "path";
 import * as vscode from 'vscode';
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions
+} from 'vscode-languageclient/node';
+
 import { GtirbEditorProvider } from './gtirbEditor';
 
+let client: LanguageClient;
+
+function getClientOptions(): LanguageClientOptions {
+    return {
+        // Register the server for plain text documents
+        documentSelector: [
+            //{ scheme: "file", language: "json" },
+            { scheme: "file", language: "gtgas" },
+        ],
+        outputChannelName: "[pygls] gtgas LanguageServer",
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contain in the workspace
+            fileEvents: vscode.workspace.createFileSystemWatcher("**/.clientrc"),
+        },
+    };
+}
+
+function isStartedInDebugMode(): boolean {
+    return process.env.VSCODE_DEBUG_MODE === "true";
+}
+
+function startLangServerTCP(addr: number): LanguageClient {
+    const serverOptions: ServerOptions = () => {
+        return new Promise((resolve /*, reject */) => {
+            const clientSocket = new net.Socket();
+            clientSocket.connect(addr, "127.0.0.1", () => {
+                resolve({
+                    reader: clientSocket,
+                    writer: clientSocket,
+                });
+            });
+        });
+    };
+
+    return new LanguageClient(
+        `tcp lang server (port ${addr})`,
+        serverOptions,
+        getClientOptions()
+    );
+}
+
+function startLangServer(
+    command: string,
+    args: string[],
+    cwd: string
+): LanguageClient {
+    const serverOptions: ServerOptions = {
+        args,
+        command,
+        options: { cwd },
+    };
+
+    return new LanguageClient(command, serverOptions, getClientOptions());
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	// Register our custom editor providers
-	context.subscriptions.push(GtirbEditorProvider.register(context));
+    if (isStartedInDebugMode()) {
+        // Development - Run the server manually
+        client = startLangServerTCP(3036);
+    } else {
+        // Production - Client is going to run the server (for use within `.vsix` package)
+        const cwd = path.join(__dirname, "..");
+        const pythonPath = vscode.workspace
+            .getConfiguration("python")
+            .get<string>("pythonPath");
+
+        if (!pythonPath) {
+            throw new Error("`python.pythonPath` is not set");
+        }
+
+        client = startLangServer(pythonPath, ["-m", "server"], cwd);
+    }
+
+    context.subscriptions.push(client.start());
+    // Register our custom editor providers
+    context.subscriptions.push(GtirbEditorProvider.register(context));
+}
+
+
+export function deactivate(): Thenable<void> {
+    return client ? client.stop() : Promise.resolve();
 }
