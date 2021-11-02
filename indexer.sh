992e6be5ac7d9f8203c5e6f3188445986b15dc58
Timothy Neale <tneale@grammatech.com>
2021-11-02 12:21:15 -0400

    Add UI listing mode to pprinter call

--- indexer.sh
+++ indexer.sh
@@ -1,57 +1,73 @@
 #!/usr/bin/env bash
 #
-DEBUG=true
+DEBUG=false
+CONTINUE=true
 
-if [[ $# -gt 0 ]]; then
+if [[ $# -lt 1 ]]; then
+	MESSAGE="No file. Usage: $0 <gtirb-file-name>"
+	CONTINUE=false
+fi
+
+if ${CONTINUE} 
+then
 	FS_PATH=$1
-	if [[ ${DEBUG} ]]; then
-		echo "GTIRB file path: ${FS_PATH}"
+	if [[ -f ${PWD}/get-isa.py ]]; then
+		ISA=`./get-isa.py ${FS_PATH}`
+	else
+		MESSAGE="get-isa.py script not found."
+		CONTINUE=false
 	fi
-	ISA=`./get-isa.py ${FS_PATH}`
+fi
+
+if ${CONTINUE} 
+then
 	if [[ "${ISA}" == "x86" || "${ISA}" == "x64" || "${ISA}" == "mips" || "${ISA}" == "arm" ]]; then
-		if [[ ${DEBUG} ]]; then
-			rm -f ${GTIRB_FOLDER}/log.txt # !!!!!!!!!!!!! tmp of course get rid of
-		fi
 		GTIRB_FOLDER=`dirname $FS_PATH`
 		GTIRB_FILE=`basename $FS_PATH`
 		CACHE_PATH="${GTIRB_FOLDER}/.vscode.${GTIRB_FILE}"
-		if [[ ${DEBUG} ]]; then
-			echo "GTIRB_FOLDER: ${GTIRB_FOLDER}" > ${GTIRB_FOLDER}/log.txt
+		GTIRB_BASE=${GTIRB_FILE%.*}
+		ISA_DIR=${CACHE_PATH}/${ISA}
+		ASM_FILE="${ISA_DIR}/${GTIRB_BASE}.gtasm"
+
+		if ${DEBUG} 
+		then
+			echo "GTIRB file path: ${FS_PATH}" > ${GTIRB_FOLDER}/log.txt
+			echo "GTIRB_FOLDER: ${GTIRB_FOLDER}" >> ${GTIRB_FOLDER}/log.txt
 			echo "GTIRB_FILE: ${GTIRB_FILE}" >> ${GTIRB_FOLDER}/log.txt
 			echo "CACHE_PATH: ${CACHE_PATH}" >> ${GTIRB_FOLDER}/log.txt
-		fi
-		# IFF it doesn't exist, create a subdir for caching files
-		if [[ ! -d ${CACHE_PATH} ]]; then
-			mkdir ${CACHE_PATH}
-		fi
-		if [[ ! -d ${CACHE_PATH} && ${DEBUG} ]]; then
-			echo "Failed to create work subdir ${CACHE_PATH}" >> ${GTIRB_FOLDER}/log.txt
-		else
-			ISA_DIR=${CACHE_PATH}/${ISA}
-			if [[ ! -d ${ISA_DIR} ]]; then
-				mkdir ${ISA_DIR}
-			fi 
-			if [[ ! -d ${ISA_DIR} && ${DEBUG} ]]; then
-				echo "Failed to create ISA subdir ${ISA_DIR}" >> ${GTIRB_FOLDER}/log.txt
-			else
-				GTIRB_BASE=${GTIRB_FILE%.*}
-				ASM_FILE="${ISA_DIR}/${GTIRB_BASE}.gtasm"
-				echo "creating asm file ${ASM_FILE}"
-				gtirb-pprinter --ir ${FS_PATH} --asm ${ASM_FILE} 
-				if [[ ! -f ${ASM_FILE} && ${DEBUG} ]]; then
-					echo "Failed to create ASM FILE ${ASM_FILE}" >> ${GTIRB_FOLDER}/log.txt
-				fi
-			fi
+			echo "ISA_DIR: ${ISA_DIR}" >> ${GTIRB_FOLDER}/log.txt
+			echo "ASM_FILE: ${ASM_FILE}" >> ${GTIRB_FOLDER}/log.txt
 		fi
 	else
-		if [[ ${DEBUG} ]]; then
-			echo "BAD ISA: ${ISA}"
-		fi
-	fi
-		
-	#touch ${ASM_FILE}
-else
-	if [[ ${DEBUG} ]]; then
-		echo "no file?"
+		MESSAGE="Bad ISA: ${ISA}."
+		CONTINUE=false
 	fi
 fi
+
+
+if ${CONTINUE} 
+then
+	if [[ -d ${ISA_DIR} && -f ${ASM_FILE} ]]; then
+		MESSAGE="Using existing assembly file ${ASM_FILE}"
+		CONTINUE=false
+	else
+		# IFF it doesn't exist, create a subdir for caching files
+		if [[ ! -d ${ISA_DIR} ]]; then
+			mkdir -p ${ISA_DIR}
+		fi
+		if [[ ! -d ${ISA_DIR} ]]; then
+			MESSAGE="Failed to create cache subdir ${ISA_DIR}"
+			CONTINUE=false
+		fi
+	fi
+fi
+
+if ${CONTINUE} 
+then
+	gtirb-pprinter --ir ${FS_PATH} --asm ${ASM_FILE} --listing-mode ui 
+	if [[ ! -f ${ASM_FILE} ]]; then
+		MESSAGE="Failed to create ASM FILE ${ASM_FILE}"
+	fi
+fi
+
+echo "${MESSAGE}"
